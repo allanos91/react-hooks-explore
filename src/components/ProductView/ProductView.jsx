dbd78e7e1427d8817c35121746df6d31dc2e6ff6
Johann <johnhansong@gmail.com>
2024-05-11 13:39:02 -0700

    phase-1 edits js

--- src/components/ProductView/ProductView.jsx
+++ src/components/ProductView/ProductView.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ProductListItem from '../ProductListItem';
 import ProductDetails from '../ProductDetails';
 import './ProductView.css';
+import { useState } from 'react'
+
 
 function ProductView({ products }) {
 
   // TODO: Replace with state variable
-  const sideOpen = true;
+  const [sideOpen, setSideOpen] = useState(true);
 
   return (
     <div className="product-view">
@@ -25,7 +27,7 @@ function ProductView({ products }) {
       <div className="product-side-panel">
         <div className="product-side-panel-toggle-wrapper">
           <div className="product-side-panel-toggle"
-               onClick={() => console.log('TOGGLE SIDE PANEL')}>
+              onClick={() => setSideOpen(!sideOpen)}>
             {sideOpen ? '>' : '<'}
           </div>
         </div>
